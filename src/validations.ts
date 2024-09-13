630e0f0096ab295216964aa79c2242d6cd591c9c
Luke Zehrung <lzehrung@gmail.com>
2024-09-12 23:27:47 -0400

    validate wip

--- src/validations.ts
+++ src/validations.ts
@@ -1,18 +1,37 @@
 import { parseDate } from './utils';
 
-export function validateData(data: any[], schema: any[], columnMappings: any): any[] {
-  const errors: any[] = [];
+export interface ValidationError<T extends ImportRow> {
+  key: keyof T;
+  message: string;
+}
+
+export interface ValidateResult<T extends ImportRow> {
+  rowIndex: number;
+  isValid: boolean;
+  errors: ValidationError<T>[];
+}
+
+export type ImportRow = { [key: string | number]: string | number };
+
+export type ValidatorFunc = (value: string | number) => { isValid: boolean; message: string };
+
+export function validateData<T extends ImportRow>(
+  data: Record<string, string | number>[],
+  schema: Record<keyof T, ValidatorFunc>,
+  columnMappings: Record<keyof T, string>
+): ValidateResult<T>[] {
+  const allErrors: ValidateResult<T>[] = [];
   data.forEach((row, rowIndex) => {
-    const errorRow = {};
-    schema.forEach((col) => {
-      const value = row[columnMappings[col.name]];
-      // if (!isValid(value, col)) {
-      //   errorRow[columnMappings[col.name]] = value;
-      // }
+    const rowErrors: ValidationError<T>[] = [];
+    Object.entries(schema).forEach(([destProp, validator]) => {
+      const srcProp = columnMappings[destProp];
+      const value = row[srcProp];
+
+      const result = validator(value);
+      if (!result.isValid) {
+        rowErrors.push({ key: destProp, message: result.message });
+      }
     });
-    if (Object.keys(errorRow).length > 0) {
-      errors.push({ ...errorRow, rowIndex });
-    }
   });
-  return errors;
+  return allErrors;
 }
