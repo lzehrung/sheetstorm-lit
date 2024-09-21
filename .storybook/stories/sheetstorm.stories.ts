2c6e17fd6546a5557fbd15ae456306e86fb3ffc5
Luke Zehrung <lzehrung@gmail.com>
2024-09-21 10:24:11 -0400

    fix modal example

--- .storybook/stories/sheetstorm.stories.ts
+++ .storybook/stories/sheetstorm.stories.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { html, TemplateResult } from 'lit';
+import { LitElement } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
 import '../../dist/index'; // Import your Lit components
 import { ValidateSchema } from '../../src/validations';
 
@@ -34,29 +37,36 @@ const schema: ValidateSchema = {
   },
 };
 
+@customElement('with-modal-wrapper')
+class WithModalWrapper extends LitElement {
+  @state()
+  private open = false;
+
+  private handleOpenClick() {
+    this.open = true;
+  }
+
+  private handleSuccess(event: CustomEvent) {
+    console.log('Data imported successfully:', event.detail);
+    this.open = false;
+  }
+
+  render(): TemplateResult {
+    return html`
+      <button @click="${this.handleOpenClick}" style="margin-bottom: 16px;">
+        Open Sheetstorm Modal
+      </button>
+      <sheetstorm-modal
+        .open="${this.open}"
+        .schema="${schema}"
+        @data-import-success="${this.handleSuccess}"
+      ></sheetstorm-modal>
+    `;
+  }
+}
+
 export const Default = (): TemplateResult =>
   html`<sheetstorm-import .schema=${schema}></sheetstorm-import>`;
 
-export const WithModal = (): TemplateResult => {
-  let open = false;
-
-  const handleOpenClick = () => {
-    open = true;
-  };
-
-  const handleSuccess = (event: CustomEvent) => {
-    console.log('Data imported successfully:', event.detail);
-    open = false;
-  };
-
-  return html`
-    <button @click="${handleOpenClick}" style="margin-bottom: 16px;">
-      Open Sheetstorm Modal
-    </button>
-    <sheetstorm-modal
-      .open="${open}"
-      .schema="${schema}"
-      @data-import-success="${handleSuccess}"
-    ></sheetstorm-modal>
-  `;
-};
+export const WithModal = (): TemplateResult =>
+  html`<with-modal-wrapper></with-modal-wrapper>`;
