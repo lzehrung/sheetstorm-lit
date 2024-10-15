4f10a0f5670043e8f4e9c8fa6c86d1aa16d44613
Luke Zehrung <lzehrung@gmail.com>
2024-10-14 22:55:47 -0400

    readme

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,8 +1,8 @@
 import { parseDate } from '../src/utils';
 
-describe('Utils Tests', () => {
-  test('parseDate should parse date correctly', () => {
-    const date = parseDate('2021-12-01', 'yyyy-MM-dd');
-    expect(date).toEqual(new Date(2021, 11, 1));
+describe('utils tests', () => {
+  test('parseDate handles expected formats', () => {
+    const date = parseDate('2021-12-02');
+    expect(date).toEqual(new Date(2021, 11, 2));
   });
 });
